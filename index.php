7971b9b3e68de96d94ff8a21bc391af0cb96d653
Mathis Parrabere <mathis.parrabere@student.junia.com>
2025-03-25 15:09:53 +0100

    Partie 2 : Afficher les données

--- index.php
+++ index.php
@@ -1,3 +1,33 @@
 <?php
 require("Model/pdo.php");
+
+$resultat = $dbPDO->prepare("SELECT * FROM etudiants");
+$resultat ->execute();
+$etudiants= $resultat->fetchAll();
+
+echo "<br><br> Les étudiants : <br><br>";
+
+foreach($etudiants as $etudiants) {
+   echo $etudiants['nom']." ".$etudiants['prenom']."<br>";
+}
+
+$resultat = $dbPDO->prepare("SELECT * FROM classes");
+$resultat ->execute();
+$classes= $resultat->fetchAll();
+
+echo "<br><br> Les classes : <br><br>";
+
+foreach($classes as $classes) {
+   echo $classes['libelle']."<br>";
+}
+
+$resultat = $dbPDO->prepare("SELECT * FROM professeurs");
+$resultat ->execute();
+$professeurs= $resultat->fetchAll();
+
+echo "<br><br> Les professeurs : <br><br>";
+
+foreach($professeurs as $professeurs) {
+   echo $professeurs['nom']." ".$professeurs['prenom']."<br>";
+}
 ?>
