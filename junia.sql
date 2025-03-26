a255905662a4bb949397199183f268010f4b7747
mathisparrabere <mathis.parrabere@student.junia.com>
2025-03-26 08:58:43 +0100

    BDD junia.sql UPDATE APRES CODE

--- junia.sql
+++ junia.sql
@@ -2,10 +2,10 @@
 -- version 5.1.2
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: Mar 20, 2024 at 07:30 PM
--- Server version: 5.7.24
--- PHP Version: 8.0.1
+-- Hôte : localhost:3306
+-- Généré le : mer. 26 mars 2025 à 07:57
+-- Version du serveur : 5.7.24
+-- Version de PHP : 8.3.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `junia`
+-- Base de données : `junia`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `classes`
+-- Structure de la table `classes`
 --
 
 CREATE TABLE `classes` (
@@ -33,17 +33,19 @@ CREATE TABLE `classes` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `classes`
+-- Déchargement des données de la table `classes`
 --
 
 INSERT INTO `classes` (`id`, `libelle`) VALUES
 (1, 'Terminal B'),
-(2, 'Seconde C');
+(2, 'Seconde C'),
+(3, 'CIR1'),
+(4, 'CIR2');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `etudiants`
+-- Structure de la table `etudiants`
 --
 
 CREATE TABLE `etudiants` (
@@ -54,19 +56,20 @@ CREATE TABLE `etudiants` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `etudiants`
+-- Déchargement des données de la table `etudiants`
 --
 
 INSERT INTO `etudiants` (`id`, `prenom`, `nom`, `classe_id`) VALUES
-(3, 'Alfred', 'Dupont', 2),
 (4, 'Albert', 'Schuman', 1),
 (5, 'Alphonse', 'Cabrol', 2),
-(6, 'Alric', 'Duportic', 1);
+(7, 'Loukas', 'Ciotti', 3),
+(8, 'Cyrvan', 'Carbonara', 3),
+(10, 'Bastos', 'Prime', 4);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `matiere`
+-- Structure de la table `matiere`
 --
 
 CREATE TABLE `matiere` (
@@ -75,19 +78,25 @@ CREATE TABLE `matiere` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `matiere`
+-- Déchargement des données de la table `matiere`
 --
 
 INSERT INTO `matiere` (`id`, `lib`) VALUES
 (1, 'SQL'),
 (2, 'WEB'),
 (3, 'Sport'),
-(4, 'Danse');
+(4, 'Danse'),
+(5, 'Espagnol'),
+(6, 'Maths'),
+(7, 'Physique'),
+(8, 'Anglaissss'),
+(9, 'gym'),
+(10, 'Electricien');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `professeurs`
+-- Structure de la table `professeurs`
 --
 
 CREATE TABLE `professeurs` (
@@ -99,39 +108,40 @@ CREATE TABLE `professeurs` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `professeurs`
+-- Déchargement des données de la table `professeurs`
 --
 
 INSERT INTO `professeurs` (`id`, `prenom`, `nom`, `id_matiere`, `id_classe`) VALUES
 (4, 'Antoine', 'De Conto', 1, 2),
 (5, 'Jesus', 'Christ', 2, 2),
-(6, 'Dora', 'l\'exploratrice', 4, 1);
+(6, 'Dora', 'l\'exploratrice', 4, 1),
+(7, 'Ralphette', 'Zogette', 3, 3);
 
 --
--- Indexes for dumped tables
+-- Index pour les tables déchargées
 --
 
 --
--- Indexes for table `classes`
+-- Index pour la table `classes`
 --
 ALTER TABLE `classes`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `etudiants`
+-- Index pour la table `etudiants`
 --
 ALTER TABLE `etudiants`
   ADD PRIMARY KEY (`id`),
   ADD KEY `fk_classe_student` (`classe_id`);
 
 --
--- Indexes for table `matiere`
+-- Index pour la table `matiere`
 --
 ALTER TABLE `matiere`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `professeurs`
+-- Index pour la table `professeurs`
 --
 ALTER TABLE `professeurs`
   ADD PRIMARY KEY (`id`),
@@ -139,45 +149,45 @@ ALTER TABLE `professeurs`
   ADD KEY `fk_prof_classe` (`id_classe`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT pour les tables déchargées
 --
 
 --
--- AUTO_INCREMENT for table `classes`
+-- AUTO_INCREMENT pour la table `classes`
 --
 ALTER TABLE `classes`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT for table `etudiants`
+-- AUTO_INCREMENT pour la table `etudiants`
 --
 ALTER TABLE `etudiants`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
--- AUTO_INCREMENT for table `matiere`
+-- AUTO_INCREMENT pour la table `matiere`
 --
 ALTER TABLE `matiere`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
--- AUTO_INCREMENT for table `professeurs`
+-- AUTO_INCREMENT pour la table `professeurs`
 --
 ALTER TABLE `professeurs`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
--- Constraints for dumped tables
+-- Contraintes pour les tables déchargées
 --
 
 --
--- Constraints for table `etudiants`
+-- Contraintes pour la table `etudiants`
 --
 ALTER TABLE `etudiants`
   ADD CONSTRAINT `fk_classe_student` FOREIGN KEY (`classe_id`) REFERENCES `classes` (`id`) ON DELETE CASCADE;
 
 --
--- Constraints for table `professeurs`
+-- Contraintes pour la table `professeurs`
 --
 ALTER TABLE `professeurs`
   ADD CONSTRAINT `fk_prof_classe` FOREIGN KEY (`id_classe`) REFERENCES `classes` (`id`) ON DELETE CASCADE,
